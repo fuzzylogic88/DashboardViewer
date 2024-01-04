a2878341e9538249c24b39d178dc91fa005dea1b
Daniel Green <daniel.green927@outlook.com>
2024-01-03 22:53:32 -0800

    ---

---
+++ DBView.py
@@ -0,0 +1,112 @@
+# DBViewer
+#
+# A small kiosk-style utility to view webpages from a SFF PC / RPi
+
+from PyQt5.QtCore import QUrl, QTimer, Qt
+from PyQt5.QtGui import QKeySequence
+from PyQt5.QtWidgets import QApplication, QShortcut
+from PyQt5.QtWebEngineWidgets import QWebEngineView as QWebView
+from PyQt5.QtWebEngineWidgets import QWebEngineSettings as QWebSettings
+from PyQt5.QtNetwork import *
+
+import platform
+import sys
+
+# URL File Locations:
+URL_FILE_PATH_WINDOWS = "C:\\DBurl.txt"
+URL_FILE_PATH_LINUX = "/home/pi/DBurl.txt"
+
+DEFAULT_DELAY_MS = 15000
+
+globalFPath = ''
+urlList = ['']
+
+class Browser(QWebView):
+    def __init__(self, urlList):
+        super(Browser,self).__init__()
+        
+        # check platform and establish filepath for URLs.
+        current_os = platform.system()
+        if current_os == 'Linux':
+            self.globalFPath = URL_FILE_PATH_LINUX
+        elif current_os == 'Windows':
+            self.globalFPath = URL_FILE_PATH_WINDOWS
+        
+        self.timers = []
+        self.urlList = load_url_from_file(self.globalFPath)
+        self.current_index = 0
+
+    def load(self,url):
+        self.setUrl(QUrl(url))
+    
+    def load_next_url(self):         
+            # move though list of dashboard URLs
+        if self.current_index < len(self.urlList):
+            url = self.urlList[self.current_index] 
+            self.load(QUrl(url))
+            self.current_index += 1
+        else:
+            # start from the beginning of the collection
+            self.current_index = 0
+        
+        # kick off a new timer, and add to collection of timers...
+        timer = QTimer(self)
+        timer.setSingleShot(True)
+        timer.timeout.connect(self.load_next_url)
+        timer.start(DEFAULT_DELAY_MS)
+        self.timers.append(timer)
+    
+    # Cancels current timer and loads the next URL in the queue
+    def skip_timers(self):
+        print("Cancelling timers and skipping to next URL")
+        for timer in self.timers:
+            timer.stop()
+            timer.timeout.disconnect()
+            timer.deleteLater()
+        self.timers.clear()
+        self.load_next_url()
+    
+    def adjustTitle(self):
+        self.setWindowTitle(self.title())
+    
+    def userAgentForUrl(self):
+        ''' Returns a User Agent that will be seen by the website. '''
+        return "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+    
+    def disableJS(self):
+        settings = QWebSettings.globalSettings()
+        settings.setAttribute(QWebSettings.JavascriptEnabled, False)
+
+def main():
+    app = QApplication(sys.argv)   
+    window = Browser(urlList)
+    
+    # creates a borderless window and displays the content fullscreen
+    window.setWindowFlags(Qt.FramelessWindowHint)
+    window.showFullScreen()
+
+    # define which keypresses to monitor for
+    window.close_shortcut = QShortcut(QKeySequence(Qt.Key_Escape),window)
+    window.skip_url_shortcut = QShortcut(QKeySequence(Qt.Key_Space),window)
+    
+    # attach our shortcuts to functions to create keyboard event handlers
+    window.close_shortcut.activated.connect(window.close)
+    window.skip_url_shortcut.activated.connect(window.skip_timers)
+    
+    window.setWindowTitle('Loading dashboard...')
+    
+    # change window title on new connections
+    window.titleChanged.connect(window.adjustTitle)
+    
+    # kick off our URL load-loop
+    window.load_next_url()
+    sys.exit(app.exec_())
+
+# reads data from filepath established during '__init__'
+def load_url_from_file(globalFPath):
+    with open(globalFPath, 'r') as file:
+        urlList = [line.strip() for line in file.readlines() if line.strip()]
+    return urlList
+
+if __name__ == "__main__":
+    main()
